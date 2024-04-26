706d6be6866accc2caea753e7e03ca5a9fed9e1d
brettnakao <157428815+brettnakao@users.noreply.github.com>
2024-04-26 14:43:44 -0700

    Update and rename Weight on a Planet.py to 3. Weight on a Planet.py

---
+++ 3. Weight on a Planet.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 22:33:26 2024
+
+@author: brettnakao
+"""
+
+"""Mass on a Planet"""
+
+#define gravity dictionary
+gravity = {"Earth": 9.8, "Mars": 3.75, "Mercury": 3.61, "Venus": 8.83, "Jupiter": 26.0, "Saturn": 11.2, "Uranus": 10.5, "Neptune": 13.3, "Pluto": 0.61}
+
+m = float(input("Enter your mass in kg:"))
+planet = input("Enter your desired planet:")
+
+weight_on_planet = mass * gravity[planet]
+
+print("Weight on planet:", weight_on_planet, "N")
