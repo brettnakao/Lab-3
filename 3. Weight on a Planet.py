af32cfa4abc8b6f6acc626dc0f3a0d41acf46984
brettnakao <157428815+brettnakao@users.noreply.github.com>
2024-04-26 14:44:35 -0700

    Update 3. Weight on a Planet.py

--- 3. Weight on a Planet.py
+++ 3. Weight on a Planet.py
@@ -11,9 +11,9 @@ Created on Mon Feb  5 22:33:26 2024
 #define gravity dictionary
 gravity = {"Earth": 9.8, "Mars": 3.75, "Mercury": 3.61, "Venus": 8.83, "Jupiter": 26.0, "Saturn": 11.2, "Uranus": 10.5, "Neptune": 13.3, "Pluto": 0.61}
 
-m = float(input("Enter your mass in kg:"))
+mass = float(input("Enter your mass in kg:"))
 planet = input("Enter your desired planet:")
 
-weight_on_planet = mass * gravity[planet]
+weight_on_planet = mass * gravity[planet] #define weight on desired planet
 
-print("Weight on planet:", weight_on_planet, "N")
+print("Weight on planet:", weight_on_planet, "N") #create string to print answer
